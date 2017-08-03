127bbe241b33dd36e23b97c841f7fb89f8209d92
marco beduschi <beduschimarco@gmail.com>
2017-08-03 13:00:47 -0300

    Created cocktails controller and basic routing

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,22 @@
+class CocktailsController < ApplicationController
+  def view
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
